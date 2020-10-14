8e21f3a44f32de898a19e955434ad98e0bafcc67
Chris Overly <cqoverly@uw.edu>
2020-10-13 20:04:44 -0700

    Moved logic to run query and parse returned student data into a dictionary, which is returned. request is added to the dict then sent to the template to be rendered.

--- connect.py
+++ connect.py
@@ -41,16 +41,61 @@ def get_cursor():
             except pyodbc.OperationalError:
                 print("Not able to connect with provided methods")
                 sys.exit()
-        finally:
-            conn.close()
-            
-                
-
-    
-  
+        # finally:
+        #     conn.close()
 
     # print('Connection to database successful')
     return conn.cursor()
+
+
+def run_query(sql):
+    cursor = get_cursor()
+    cursor.execute(sql, sid)
+    data = [s for s in cursor]
+    return data
+            
+
+def get_student_data(sql: str, sid: str)-> dict:
+
+    cursor = get_cursor()
+    cursor.execute(sql, sid)
+    data = [s for s in cursor][0]
+
+    #parse data into student attributes
+    student_no = data[0]
+    split_name = data[1].split(sep=',')
+    last_name, first_name = split_name[0], split_name[1]
+    pref_first_name = data[2].strip()
+    qtrs_used = data[3]
+    uw_email = data[4]
+    alt_email = data[5]
+    campus = data[6]
+    major_abbr = data[7]
+    deg_pathway = data[8]
+    deg_level = data[9]
+    deg_type = data[10]
+    deg_status = data[11]
+    deg_earned_yr = data[12]
+    deg_earned_qtr = data[13]
+
+    if pref_first_name:
+        first_name = pref_first_name
+
+    # package data for injection into template
+    student_info = {
+            "sid": student_no,
+            "name": f'{first_name} {last_name}',
+            "qtrs_used": qtrs_used,
+            "uw_email": uw_email,
+            "other_email": alt_email,
+            "campus": campus,
+            "major": major_abbr,
+            "degree": f'{deg_pathway}-{deg_level}{deg_type}',
+            "degree_status": deg_status,
+            "degree_earned": f'{deg_earned_qtr}-{deg_earned_yr}'
+    }
+
+    return student_info
 
 
 if __name__ == '__main__':
--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 
 import sql_scripts
-from connect import get_cursor
+import connect
 
 app = FastAPI()
 
@@ -32,45 +32,8 @@ async def read_item(request: Request, id:str):
 async def get_student_info_from_sid(request: Request, sid: str=None):
     if sid:
         sql = sql_scripts.info_from_sid
-        cursor = get_cursor()
-        cursor.execute(sql, sid)
-        data = [s for s in cursor][0]
-
-        #parse data into student attributes
-        student_no = data[0]
-        split_name = data[1].split(sep=',')
-        last_name, first_name = split_name[0], split_name[1]
-        pref_first_name = data[2].strip()
-        qtrs_used = data[3]
-        uw_email = data[4]
-        alt_email = data[5]
-        campus = data[6]
-        major_abbr = data[7]
-        deg_pathway = data[8]
-        deg_level = data[9]
-        deg_type = data[10]
-        deg_status = data[11]
-        deg_earned_yr = data[12]
-        deg_earned_qtr = data[13]
-
-        if pref_first_name:
-            first_name = pref_first_name
-
-        # package data for injection into template
-        student_info = {
-                "request": request,
-                "sid": student_no,
-                "name": f'{first_name} {last_name}',
-                "qtrs_used": qtrs_used,
-                "uw_email": uw_email,
-                "other_email": alt_email,
-                "campus": campus,
-                "major": major_abbr,
-                "degree": f'{deg_pathway}-{deg_level}{deg_type}',
-                "degree_status": deg_status,
-                "degree_earned": f'{deg_earned_qtr}-{deg_earned_yr}'
-        }
-        # print(student_info)
+        student_info = connect.get_student_data(sql, sid)
+        student_info["request"] = request
 
         return(templates.TemplateResponse("student_info.html", student_info))
     else:
