603627949e80d6e41a59defdb49da8b1d6f572e6
Chris Overly <cqoverly@uw.edu>
2022-02-20 05:45:56 -0800

    Commented out sqlalchemy import since not used and in venv of some machines.

--- views/rooms.py
+++ views/rooms.py
@@ -7,7 +7,7 @@ from fastapi.responses import HTMLResponse
 
 # from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
-from sqlalchemy import tablesample
+# from sqlalchemy import tablesample
 
 # from starlette.routing import request_response
 
